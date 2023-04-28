1efb0330e3926235cde79bdaec9bf157dc6d1743
welpo <welpo@users.noreply.github.com>
2023-04-28 16:20:42 +0200

    🐛 fix: remove flash when navigating in dark-mode
    The `initialize-theme.js` script takes care of the following:
    
      1. If there is a stored theme value in the localStorage, set the theme
     based on that value.
      2. If there is no stored theme value, check the  user's system
      preference (dark or light) and set the theme accordingly.
    
    The new `main.js` takes care of the actual theme switching and listening
    to system preference changes (if the user has not manually set a theme).
    
    Now the icons are stored in CSS, and are set according to the current
    theme. This allows for having different icons that dynamically switch.
    
    Additionally, wraps social and navigation elements in ul/li.
    
    Fixes #76

---
+++ static/js/initialize_theme_min.js
@@ -0,0 +1,1 @@
+!function(){let e=localStorage.getItem("theme");if(e)document.documentElement.setAttribute("data-theme",e);else{let t=window.matchMedia("(prefers-color-scheme: dark)").matches;document.documentElement.setAttribute("data-theme",t?"dark":"light")}}();
