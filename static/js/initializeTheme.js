a4e48b48fdc010bc22dfa41d00baaab317e7733c
welpo <welpo@users.noreply.github.com>
2023-08-02 23:52:28 +0200

    💡 chore: add comments

--- static/js/initializeTheme.js
+++ static/js/initializeTheme.js
@@ -1,9 +1,17 @@
 (function () {
+    // Get the current theme from the browser's local storage.
+    // This allows the user's theme preference to persist across sessions.
     const currentTheme = localStorage.getItem('theme');
+
+    // Check if the current theme is stored in local storage.
     if (currentTheme) {
+        // If a theme is found in local storage, apply it to the document.
         document.documentElement.setAttribute('data-theme', currentTheme);
     } else {
+        // If no theme is found in local storage, determine if the user's system prefers a dark color scheme.
         const isSystemDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+        // Set the document's theme attribute to match the system preference.
         document.documentElement.setAttribute('data-theme', isSystemDark ? 'dark' : 'light');
     }
 })();
