5dd38c8b6c071f9d6a25eda12457ed97efb74eb0
blaine-t <blaine-t@bathost.net>
2024-09-06 01:12:41 -0500

    Add passive event listeners to search

--- static/js/searchElasticlunr.js
+++ static/js/searchElasticlunr.js
@@ -2685,7 +2685,7 @@ window.onload = function () {
         event.stopPropagation(); // Prevents tapping through the modal.
     }
     searchModal.addEventListener('click', handleModalInteraction);
-    searchModal.addEventListener('touchend', handleModalInteraction);
+    searchModal.addEventListener('touchend', handleModalInteraction, {passive: true});
 
     // Close modal when pressing escape.
     document.addEventListener('keydown', function (event) {
@@ -2710,7 +2710,7 @@ window.onload = function () {
     // Clicking/tapping the search button opens the modal.
     searchButton.addEventListener('mouseover', loadSearchIndex);
     searchButton.addEventListener('click', openSearchModal);
-    searchButton.addEventListener('touchstart', openSearchModal);
+    searchButton.addEventListener('touchstart', openSearchModal, {passive: true});
 
     let searchIndexPromise = null;
     function loadSearchIndex() {
@@ -3105,7 +3105,7 @@ window.onload = function () {
         resultDivs.forEach((div) => {
             // Remove existing listener to avoid duplicates.
             div.removeEventListener('touchstart', handleTouchStart);
-            div.addEventListener('touchstart', handleTouchStart);
+            div.addEventListener('touchstart', handleTouchStart, {passive: true});
         });
     }
 
